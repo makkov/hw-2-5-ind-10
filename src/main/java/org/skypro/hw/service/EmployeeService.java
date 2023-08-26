55642c6acdf0474b19e7c87a3b83236e96c84919
maks <maxim_86nva@mail.ru>
2023-08-26 14:07:51 +0300

    начало тестов

--- src/main/java/org/skypro/hw/service/EmployeeService.java
+++ src/main/java/org/skypro/hw/service/EmployeeService.java
@@ -14,17 +14,6 @@ public class EmployeeService {
 
     private final List<Employee> employees = new ArrayList<>();
 
-    public EmployeeService() {
-        employees.add(new Employee("Иван", "Иванов", 1000.0, 1));
-        employees.add(new Employee("Иван1", "Иванов1", 999.9, 1));
-        employees.add(new Employee("Иван2", "Иванов2", 8.50, 1));
-
-        employees.add(new Employee("Пётр", "Петров", 8.50, 2));
-
-        employees.add(new Employee("Илья", "Ильин", 777.50, 3));
-        employees.add(new Employee("Илья1", "Ильин1", 88.8, 3));
-    }
-
     private final static int MAX_SIZE = 2;
 
     public Employee add(String firstName, String lastName, double salary, int departmentId) {
